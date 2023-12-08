73fea96eb5ad0d757291bdb9ebb23dd6147eaeba
binho <binho@binhos-MacBook-Pro.local>
2023-12-08 16:27:14 -0600

    Add new logo images

--- README.rst
+++ README.rst
@@ -1,1 +1,6 @@
+.. image:: https://github.com/fkmendes/PhyloJunction/blob/main/docs/source/images/pj_logo_github.png
+
+   :align: right
+   :alt: PhyloJunction
+
 # PhyloJunction
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,11 @@ napoleon_attr_annotations = True
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_logo = 'images/pj_logo_doc.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 
 rst_prolog = """
